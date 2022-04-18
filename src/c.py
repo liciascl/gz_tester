f617998916069ca4fb19d28e65a2fc12cd1b6bf0
liciascl <licia.sales@hotmail.com>
2022-04-18 19:38:19 -0300

    C mapeando os pontos da pista

--- src/c.py
+++ src/c.py
@@ -25,12 +25,12 @@ class Spy:
     def __init__(self):   
 
                 
-        self.estado = [("PRIMEIRO_PONTO",-3.12,0.30),("SEGUNDO_PONTO",-4.64, 2.058), ("TERCEIRO_PONTO",-2.88, -0.914),("QUARTO_PONTO",-4.10,-2.89),("QUINTO_PONTO",1.38,-1.25),("SEXTO_PONTO",1.38, 0.80),("PONTO_ZERO", 0, 0)]
+        self.estado = [("PRIMEIRO_PONTO",-2.54,-0.07),("SEGUNDO_PONTO",-4.64, 2.058), ("TERCEIRO_PONTO",-2.88, -0.914),("QUARTO_PONTO",-3.30,-2.20),("QUINTO_PONTO",5.04,-0.7),("SEXTO_PONTO",2.36,-2.17),("PONTO_ZERO", 0.8, -0.10)]
         self.hertz = 250
         self.rate = rospy.Rate(self.hertz)
         self.model_name = "mybot"
         self.relative_entity_name = "world"
-        
+        self.status=0
 
     def laser_callback(self, msg):
          self.laser_msg = msg
@@ -40,48 +40,65 @@ class Spy:
 
         return posicao_atual.pose.position.x, posicao_atual.pose.position.y
 
+
+    # def search_point(self):
+    #     posicao_atual = self.get_position()
+    #     for x in len(self.estado):
+    #         lista=self.estado[x]
+    #         if math.isclose(posicao_atual[0] in lista) and math.isclose(posicao_atual[1] in lista):
+    #             return self.estado[x][0]
+
+
+
+
+
     def conceito_c(self):
         estados=len(self.estado)
-        status = 0
-        if status == 10:
+        
+        
+        if self.status == 10:
             return "PERCORREU TODA A PISTA"
 
-        for status in range (estados):
-            posicao_esperada = self.estado[status]           
-
         
+        posicao_esperada = self.estado[self.status]
+                   
+
+        print(posicao_esperada)  
         posicao_atual = self.get_position()
-        if np.isclose(posicao_esperada[1], posicao_atual[0], atol=1).any() and np.isclose(posicao_esperada[2], posicao_atual[1], atol=1).any():
+        print(posicao_atual) 
+        #print(posicao_esperada[1], posicao_atual[0])
+        if math.isclose(posicao_esperada[1], posicao_atual[0], rel_tol=0.5) and math.isclose(posicao_esperada[2], posicao_atual[1], rel_tol=0.5):
+            
             if posicao_esperada[0] == "PRIMEIRO_PONTO":
-                status = 1
+                self.status = 1
                 return posicao_esperada[0]
 
             if posicao_esperada[0] == "SEGUNDO_PONTO":
-                status = 2
+                self.status = 2
                 return posicao_esperada[0]
 
             if posicao_esperada[0] == "TERCEIRO_PONTO":
-                status = 3
+                self.status = 3
                 return posicao_esperada[0]
 
             if posicao_esperada[0] == "QUARTO_PONTO":
-                status = 4
+                self.status = 4
                 return posicao_esperada[0]
 
             if posicao_esperada[0] == "QUINTO_PONTO":
-                status = 5
+                self.status = 5
                 return posicao_esperada[0]
         
             if posicao_esperada[0] == "SEXTO_PONTO":
-                status = 6
+                self.status = 6
                 return posicao_esperada[0]
 
             if posicao_esperada[0] == "PONTO_ZERO":
-                status = 10
+                self.status = 10
                 return posicao_esperada[0]
 
         else:
-            return "Ainda não cumpriu uma volta completa"
+            return  self.status
         
 
 
